dc713fb82373c41e2ce8e0e34aa93351f7cd5af3
kynmh69 <hiroki.kayanuma@gmail.com>
2021-04-29 16:05:56 +0900

    initial commt

---
+++ SwiftUI_LoadingView/SwiftUI_LoadingView/SwiftUI_LoadingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LoadingViewApp.swift
+//  SwiftUI_LoadingView
+//
+//  Created by Hiroki Kayanuma on 2021/04/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LoadingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
